42108b0b943fd4ac570b5d0321c7a04b15f565c7
hasan <hasangencx@hotmail.com>
2019-06-06 14:15:07 +0300

    update client

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,35 @@
-
 import React, { Component } from 'react';
 import { socket } from './config';
-
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.ws = new WebSocket('ws://' + socket.url);
-    this.ws.addEventListener('message', function(e) {
-       var msg = JSON.parse(e.data);
-       console.log(msg);  
-    });
-  
+    this.ws.onmessage = function(e) {
+      var msg = JSON.parse(e.data);
+      console.log(msg);
+    };
+
   }
-  
+
   componentDidMount() {
-    
+
   }
-  
+
   send() {
-      this.ws.send(
-        JSON.stringify({
-          email: "hasn maiil",
-          username: "hasanusername",
-          message: "hello everyone" 
-        })
-      );
+    this.ws.send(
+      JSON.stringify({
+        x: 61,
+        y: 34
+      })
+    );
   }
   render() {
     return (
       <button onClick={() => this.send()} >
         slm
-    </button>
+      </button>
     );
   }
 }
