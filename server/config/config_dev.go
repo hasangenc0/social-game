bf6f6d8a309f519e9f802bee0be739f268b9ff2a
hasan <hasangencx@hotmail.com>
2019-06-06 14:15:48 +0300

    confgi

---
+++ server/config/config_dev.go
@@ -0,0 +1,12 @@
+// +build dev
+
+package config
+
+const (
+	MONGODB_HOST            = ""
+	MONGODB_DATABASE        = ""
+	MONGODB_CONNECTION_POOL = 0
+	API_PORT                = "8080"
+	SOCKET_PORT							= "8080"
+	CLIENT_DIR 							= "../../client/build"
+)
---
+++ server/config/config_testing.go
@@ -0,0 +1,9 @@
+// +build testing
+
+package config
+
+const (
+	API_PORT                = "8080"
+	SOCKET_PORT							= "8080"
+	CLIENT_DIR 							= "../../client/build"
+)
